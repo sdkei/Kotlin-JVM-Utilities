ae97bdc6fd6330fc7478f39e7a0b735fcb25c63a
sdkei <sdkei0xb@gmail.com>
2018-10-21 16:29:54 +0900

    feat: add function radToDeg and degToRad, and their tests.

--- src/main/kotlin/io/github/sdkei/kotlin_jvm_utils/MathUtils.kt
+++ src/main/kotlin/io/github/sdkei/kotlin_jvm_utils/MathUtils.kt
@@ -1,5 +1,6 @@
 package io.github.sdkei.kotlin_jvm_utils
 
+import kotlin.math.PI
 import kotlin.math.abs
 
 /**
@@ -12,3 +13,25 @@ import kotlin.math.abs
 infix fun Int.modPositive(divisor: Int): Int =
         (this % divisor)
                 .letIfNot({ it >= 0 }) { it + abs(divisor) }
+
+/**
+ * Converts degree to radian.
+ *
+ * @return radian which is greater or equals than -1.0 * PI and less than 1.0 * PI.
+ */
+fun Double.degToRad(): Double =
+        ((this / 180.0) % 2.0).let {
+            if (it > 1.0) it - 2.0
+            else if (it <= -1.0) it + 2.0
+            else it
+        } * PI
+
+/**
+ * Converts radian to degree.
+ *
+ * @return degree which is greater or equals than 0.0 and less than 360.0.
+ */
+fun Double.radToDeg(): Double =
+        (this % (2.0 * PI)).letIf({ it < 0.0 }) {
+            it + 2.0 * PI
+        } / PI * 180.0
--- src/test/kotlin/io/github/sdkei/kotlin_jvm_utils/MathUtilsTest.kt
+++ src/test/kotlin/io/github/sdkei/kotlin_jvm_utils/MathUtilsTest.kt
@@ -2,6 +2,7 @@ package io.github.sdkei.kotlin_jvm_utils
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import kotlin.math.PI
 
 class MathUtilsTest {
 
@@ -34,5 +35,39 @@ class MathUtilsTest {
         assertEquals(2, -4 modPositive -3); assertEquals(-1, -4 % -3)
         assertEquals(1, -5 modPositive -3); assertEquals(-2, -5 % -3)
         assertEquals(0, -6 modPositive -3); assertEquals(0, -6 % -3)
+    }
+
+    @Test
+    fun test_degToRad() {
+        val delta = 1.0 / 0x1000000
+        assertEquals(-0.5 * PI, (-450.0).degToRad(), delta)
+        assertEquals(+0.0 * PI, (-360.0).degToRad(), delta)
+        assertEquals(+0.5 * PI, (-270.0).degToRad(), delta)
+        assertEquals(+1.0 * PI, (-180.0).degToRad(), delta)
+        assertEquals(-0.5 * PI, (-90.0).degToRad(), delta)
+        assertEquals(+0.0 * PI, (-0.0).degToRad(), delta)
+        assertEquals(+0.0 * PI, 0.0.degToRad(), delta)
+        assertEquals(+0.5 * PI, 90.0.degToRad(), delta)
+        assertEquals(+1.0 * PI, 180.0.degToRad(), delta)
+        assertEquals(-0.5 * PI, 270.0.degToRad(), delta)
+        assertEquals(+0.0 * PI, 360.0.degToRad(), delta)
+        assertEquals(+0.5 * PI, 450.0.degToRad(), delta)
+    }
+
+    @Test
+    fun test_radToDeg() {
+        val delta = 1.0 / 0x1000000
+        assertEquals(270.0, (-2.5 * PI).radToDeg(), delta)
+        assertEquals(0.0, (-2.0 * PI).radToDeg(), delta)
+        assertEquals(90.0, (-1.5 * PI).radToDeg(), delta)
+        assertEquals(180.0, (-1.0 * PI).radToDeg(), delta)
+        assertEquals(270.0, (-0.5 * PI).radToDeg(), delta)
+        assertEquals(0.0, (-0.0 * PI).radToDeg(), delta)
+        assertEquals(0.0, (+0.0 * PI).radToDeg(), delta)
+        assertEquals(90.0, (+0.5 * PI).radToDeg(), delta)
+        assertEquals(180.0, (+1.0 * PI).radToDeg(), delta)
+        assertEquals(270.0, (+1.5 * PI).radToDeg(), delta)
+        assertEquals(0.0, (+2.0 * PI).radToDeg(), delta)
+        assertEquals(90.0, (+2.5 * PI).radToDeg(), delta)
     }
 }
